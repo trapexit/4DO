4ffe157b7c0965a826c9956912b939bc61da97c1
hankystyles <hankystyles@f4ef83f8-773f-0410-a807-d18eb2b73810>
2007-12-23 23:37:41 +0000

    documentation for the file class
    git-svn-id: https://svn.code.sf.net/p/fourdo/code/trunk@40 f4ef83f8-773f-0410-a807-d18eb2b73810

--- code/filesystem/file.h
+++ code/filesystem/file.h
@@ -9,12 +9,64 @@ class File
 		File(const char *rom);
 		~File();
 
+		// 
+		// openFile:
+		//     opens a file on a 3do disc, and prepares to read
+		//     it's contents
+		// 
+		// arguments:
+		//     1) const char *path (IN): the 3do filesystem path 
+		//         to the file to be opened
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool openFile(const char *path);
+
+		// 
+		// closeFile:
+		//     closes the currently opened file, if there is one
+		// 
+		// arguments:
+		//     none
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool closeFile();
 
+		// 
+		// read:
+		//     reads an arbitrary number of bytes from the currently
+		//     opened file.
+		// 
+		// arguments:
+		//     1) uint8_t *buf (OUT): a buffer that will be filled with
+		//         the data read from teh file
+		//     2) const uint32_t bufLength (IN): the length of the buffer
+		//         in parameter 1.  also constitutes the maximum number
+		//         of bytes that will be read from the file.
+		//     3) uint32_t *bytesRead (OUT): the number of bytes that 
+		//         were actually read from the file.
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool read(uint8_t *buf, const uint32_t bufLength, uint32_t *bytesRead);
+
 		bool readLine(uint8_t *buf, const uint32_t bufLength, uint32_t *bytesRead);
 
+		// 
+		// getFileSize: 
+		//     returns the file size of the currently opened file
+		// 
+		// arguments:
+		//     none
+		// 
+		// return value:
+		//     the number of bytes in the currently opened file.  
+		//     will return 0 if there is no file currently open
+		// 
 		uint32_t getFileSize();
 	
 	private:
