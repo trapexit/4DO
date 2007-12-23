8fab2bb4ea6b96d9ec02c2082e6b14e749e54c26
hankystyles <hankystyles@f4ef83f8-773f-0410-a807-d18eb2b73810>
2007-12-23 23:25:52 +0000

    documentation for directory.h
    git-svn-id: https://svn.code.sf.net/p/fourdo/code/trunk@39 f4ef83f8-773f-0410-a807-d18eb2b73810

--- code/filesystem/directory.h
+++ code/filesystem/directory.h
@@ -9,12 +9,63 @@ class Directory
 		Directory(const char *rom);
 		~Directory();
 
+		// 
+		// openDirectory: 
+		//     opens a directory at the path specified
+		//     if this call succeeds, you will be ready to 
+		//     enumerate directory contents
+		// 
+		// arguments:
+		//     1) const char *path (IN): the filesystem path
+		//         to the directory to open
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool openDirectory(const char *path);
+
+		// 
+		// closeDirectory: 
+		//     closes a directory the open directory, if any
+		// 
+		// arguments:
+		//     none
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool closeDirectory();
 
+		// 
+		// changeDirectory:
+		//     changes the currently open directory to the one
+		//     specified in the path
+		// 
+		// arguments:
+		//     1) const char *path (IN): the filesystem path
+		//         to the directory to be changed to
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool changeDirectory(const char *path);
+
 		bool getDirectory(const char *path);
 
+		// 
+		// enumerateDirectory:
+		//     enumerates the contents of the currently opened
+		//     directory.  the details of the DirectoryEntry param
+		//     can be found in filesystem.h
+		// 
+		// arguments:
+		//     1) DirectoryEntry *de (OUT): will contain the information
+		//         about the next directory entry in the currently opened
+		//         directory after completion
+		// 
+		// return value:
+		//     true on success, false otherwise
+		// 
 		bool enumerateDirectory(DirectoryEntry *de);
 	private:
 		// 
